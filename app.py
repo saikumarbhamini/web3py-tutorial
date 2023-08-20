d0a7c478efdf8358009d91d6daf701f627ceba35
saikumar.bhamini <saikumar.bhamini@acsicorp.com>
2023-08-20 15:27:07 +0530

    Create a env.py file to store environment variables and removed hardcoded url.

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from web3 import Web3
 
+from env import INFURA_URL
 
-http_provider_url = 'https://mainnet.infura.io/v3/568055971b6e4859bb3265e35d24fba8'
-web3 = Web3(Web3.HTTPProvider(http_provider_url))
+infura_url = INFURA_URL
+web3 = Web3(Web3.HTTPProvider(infura_url))
 print(f"Connected: {web3.is_connected()}")
 json_block_data = Web3.to_json(web3.eth.get_block('latest'))
 acc = Web3.eth.account.create()
